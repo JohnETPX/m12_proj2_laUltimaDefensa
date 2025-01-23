0dd745cd930d092641192a61c22b0b72f67a5326
JohnETPX <john.garciano@estudiant.fjaverianas.com>
2025-01-23 23:31:13 +0100

    ENTREGA FINAL

---
+++ app/src/main/java/io/github/johnetpx/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package io.github.johnetpx.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
